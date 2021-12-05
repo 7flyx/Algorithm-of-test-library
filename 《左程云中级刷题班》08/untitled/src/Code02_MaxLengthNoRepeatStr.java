f9588d67e4617bc42dabd66974b7b55d52861166
7flyx <2647282717@qq.com>
2021-12-05 22:22:35 +0800

    最小编辑代价等

--- 《左程云中级刷题班》08/untitled/src/Code02_MaxLengthNoRepeatStr.java
+++ 《左程云中级刷题班》08/untitled/src/Code02_MaxLengthNoRepeatStr.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * Created by Terry
  * User: Administrator
@@ -13,6 +16,40 @@
  */
 public class Code02_MaxLengthNoRepeatStr {
     public static void main(String[] args) {
-
+        String str = "pwwkew";
+        System.out.println(getNoRepeatSubStr(str));
     }
+
+    public static String getNoRepeatSubStr(String str) {
+        if (str == null || str.length() == 0) {
+            return "";
+        }
+
+        //只要提到关于子串的问题，大概率就是“以当前字符结尾，会怎么样……”
+        int N = str.length();
+        char[] ch = str.toCharArray();
+        int[] dp = new int[N];
+        HashMap<Character, Integer> map = new HashMap<>(); //记录上一次出现这个字符的位置
+        map.put(ch[0], 0);
+        dp[0] = 1;
+        int maxIndex = 0; //目前最长子串的下标值
+        for (int i = 1; i < N; i++) {
+            if (!map.containsKey(ch[i])) {
+                dp[i] = 1 + dp[i - 1]; //暂时还没出现当前字符的位置，就是上一个字符的dp+1
+                map.put(ch[i], i);
+            } else {
+                //前面已经出现过这个字符，则需要和i - 1-【i-1】的值比较
+                //i-1-dp[i-1]的值，就是以上一个字符结尾的子串，有多长。再和ch[i]上次出现的位置进行比较
+                //二者取离i位置最近的那个，就是瓶颈
+                dp[i] = i - Math.max( i - 1 - dp[i - 1], map.get(ch[i]));
+                map.put(ch[i], i); //更新当前字符的下标
+            }
+            if (dp[i] > dp[maxIndex]) {
+                maxIndex = i; //更新最长子串的下标值
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return str.substring(maxIndex - dp[maxIndex] + 1, maxIndex + 1);
+    }
+
 }
--- 《左程云中级刷题班》08/untitled/src/Code03_MinCostEdit.java
+++ 《左程云中级刷题班》08/untitled/src/Code03_MinCostEdit.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by Terry
  * User: Administrator
@@ -16,6 +18,100 @@
  */
 public class Code03_MinCostEdit {
     public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 = "adc";
+        int ic = 5;
+        int dc = 3;
+        int rc = 100;
+        System.out.println(minCostEdit1(str1, str2, ic, dc, rc));
+        System.out.println(minCostEdit2(str1, str2, ic, dc, rc));
+    }
 
+    /**
+     * 经典的dp
+     *
+     * @param str1 字符串1
+     * @param str2 字符串2
+     * @param ic   插入代价
+     * @param dc   删除代价
+     * @param rc   替换代价
+     * @return 返回最小编辑代价
+     */
+    public static int minCostEdit1(String str1, String str2, int ic, int dc, int rc) {
+        if (str1 == null || str2 == null || str1.equals(str2)) {
+            return 0;
+        }
+
+        int N = str1.length();
+        int M = str2.length();
+        //横向 第一个是空串，纵向第一个也是空串
+        //纵向是str1，横向是str2.从str1转换str2
+        int[][] dp = new int[N + 1][M + 1]; //多开辟一个位置，用作空串
+        for (int i = 1; i <= N; i++) {
+            dp[i][0] = dc * i; //第一列，都是删除的代价。从一个字符串，删除为空串
+        }
+        for (int j = 1; j <= M; j++) {
+            dp[0][j] = ic * j; //第一行，都是插入的代价。从一个空串开始插入
+        }
+
+        //填写普遍位置
+        for (int i = 1; i <= N; i++) { //行
+            for (int j = 1; j <= N; j++) { //列
+                /*
+                    几种情况：str1转换为str2.
+                    dp[i][j]表示 从str1（0……i）转换为str2（0……j）的代价
+                    1 以下两行就是str（0……i-1）转化为str2（j）的代价，再加上dc的代价
+                    2 str1（0……i）转换为str2（0……j-1）的代价，再加上ic的代价
+                    3. 当前位置str1[i] = str2[j] 直接拿左上角的值【i-1】【j-1】
+                    4. 当前位置的字符不相等，加上替换字符的代价
+                 */
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] + rc;//替换字符的代价
+                }
+                //dp[i][j]表示 从str1（0……i）转换为str2（0……j）的代价
+                //以下两行就是str（0……i-1）转化为str2（j）的代价，再加上dc的代价
+                //str1（0……i）转换为str2（0……j-1）的代价，再加上ic的代价
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + dc);
+                dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + ic);
+            }
+        }
+        return dp[N][M]; //返回右下角的值
+    }
+
+    /**
+     * 空间压缩，从上到下更新的，依赖于左边、上边和左上角的值。
+     * 所以只需建立一个数组，从上到下，从左到右，滚动刷新即可
+     */
+    public static int minCostEdit2(String str1, String str2, int ic, int dc, int rc) {
+        if (str1 == null || str2 == null) {
+            return 0;
+        }
+        int N = str1.length();
+        int M = str2.length();
+        int[] dp = new int[M + 1]; //以str2作为横向
+        //dp[0] = 0; //空串的编辑代价
+        for (int j = 1; j <= M; j++) {
+            dp[j] = j * ic;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            int leftUp = dp[0]; //左上角的代价【i-1】【j-1】
+            int up = 0; //上方的代价
+            dp[0] = i * dc; //第一列的删除代价
+            for (int j = 1; j <= M; j++) { //dp[j]没更新之前，就是dp[i-1][j]
+                up = dp[j]; //上方的值
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[j] = leftUp;
+                } else {
+                    dp[j] = leftUp + rc; //加上替换的代价
+                }
+                dp[j] = Math.min(dp[j], dp[j - 1] + dc);
+                dp[j] = Math.min(dp[j], up + ic);
+                leftUp = up; //进入下一列，提前更新左上角的代价
+            }
+        }
+        return dp[M];
     }
 }
