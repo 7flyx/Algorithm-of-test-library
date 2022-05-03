e47df888e206bf254d515e1d7cf1ff6342e8ffe4
7flyx <2647282717@qq.com>
2022-05-03 20:37:33 +0800

    二叉树节点

---
+++ 体系学习班/untitled/src/class11/TreeNode.java
@@ -0,0 +1,18 @@
+package class11;
+
+/**
+ * Created by Terry
+ * User: Administrator
+ * Date: 2022-05-03
+ * Time: 18:25
+ * Description: 二叉树节点
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
