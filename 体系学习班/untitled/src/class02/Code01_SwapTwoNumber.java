1c490f37442b23f8e5271d0e9cf2f6c680058cfe
7flyx <2647282717@qq.com>
2022-05-23 21:07:59 +0800

    不用额外遍历，交换两个数

--- 体系学习班/untitled/src/class02/Code01_SwapTwoNumber.java
+++ 体系学习班/untitled/src/class02/Code01_SwapTwoNumber.java
@@ -1,5 +1,7 @@
 package class02;
 
+import java.util.Arrays;
+
 /**
  * Created by Terry
  * User: Administrator
@@ -8,13 +10,43 @@ package class02;
  * Description: 不用额外变量，交换两个数据
  */
 public class Code01_SwapTwoNumber {
-    public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
+    static class Solution {
+        public void swap(int[] arr) {
+            int tmp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = tmp;
+        }
+    }
 
-        num1 = num1 ^ num2;
-        num2 = num1 ^ num2;
-        num1 = num1 ^ num2;
-        System.out.println(num1 + " " + num2);
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int testTime = 1000;
+        int length = 2;
+        int range = 10000;
+        boolean success = true;
+        for (int i = 0; i < testTime && success; i++) {
+            int[] arr = new int[length];
+            arr[0] = (int) (Math.random() * range);
+            arr[1] = (int) (Math.random() * range);
+            int[] copy = Arrays.copyOf(arr, arr.length);
+            int[] copy2 = Arrays.copyOf(arr, arr.length);
+            test(arr);
+            solution.swap(copy);
+            if (arr[0] != copy[0] || arr[1] != copy[1]) {
+                System.out.println("测试数据: " + Arrays.toString(copy2));
+                System.out.println("期望输出： " + Arrays.toString(arr));
+                System.out.println("实际输出： " + Arrays.toString(copy));
+                success = false;
+            }
+        }
+        if(success) {
+            System.out.println("测试通过");
+        }
+    }
+
+    public static void test(int[] arr) {
+        arr[0] = arr[0] ^ arr[1];
+        arr[1] = arr[0] ^ arr[1];
+        arr[0] = arr[0] ^ arr[1];
     }
 }
